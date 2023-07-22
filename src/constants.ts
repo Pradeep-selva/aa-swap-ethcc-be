9c9b968b02adc303fd53d6dbaa7f4a19e53391e1
Rahul Tripathi <rahultripathidev@gmail.com>
2023-07-22 16:03:48 +0530

    ft: add deployer abi

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,7 @@
 import { ethers } from "ethers";
+
+export const SAFE_DEPLOYER_ADDRESS = "0x797c1228Ec6EECD03E16d40dD722add49E5209EE"
+
 const SafeAAModuleABI = [
   {
     inputs: [
@@ -184,10 +187,161 @@ const SafeMultiSendABI = [
     type: "function",
   },
 ];
-
+const DeployerAbi = [
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "_module",
+        type: "address",
+      },
+    ],
+    stateMutability: "nonpayable",
+    type: "constructor",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    name: "InvalidMultiSendCall",
+    type: "error",
+  },
+  {
+    inputs: [],
+    name: "InvalidMultiSendInput",
+    type: "error",
+  },
+  {
+    inputs: [],
+    name: "InvalidOwner",
+    type: "error",
+  },
+  {
+    inputs: [],
+    name: "OnlyOwner",
+    type: "error",
+  },
+  {
+    inputs: [],
+    name: "OnlySubAccountRegistry",
+    type: "error",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: "address",
+        name: "owner",
+        type: "address",
+      },
+      {
+        indexed: true,
+        internalType: "address",
+        name: "consoleAddress",
+        type: "address",
+      },
+    ],
+    name: "brahConsoleDeployed",
+    type: "event",
+  },
+  {
+    inputs: [],
+    name: "GNOSIS_MULTISEND",
+    outputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "VERSION",
+    outputs: [
+      {
+        internalType: "string",
+        name: "",
+        type: "string",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address[]",
+        name: "_owners",
+        type: "address[]",
+      },
+      {
+        internalType: "uint256",
+        name: "_threshold",
+        type: "uint256",
+      },
+      {
+        internalType: "address",
+        name: "_feeToken",
+        type: "address",
+      },
+    ],
+    name: "deployConsoleAccount",
+    outputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "module",
+    outputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "bytes32",
+        name: "ownersHash",
+        type: "bytes32",
+      },
+    ],
+    name: "ownerSafeCount",
+    outputs: [
+      {
+        internalType: "uint96",
+        name: "safeCount",
+        type: "uint96",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+];
 export const SafeAAModuleInterface = new ethers.utils.Interface(
   SafeAAModuleABI
 );
 export const SafeMultiSendInterface = new ethers.utils.Interface(
   SafeMultiSendABI
 );
+
+export const DeployerInterface = new ethers.utils.Interface(DeployerAbi)
