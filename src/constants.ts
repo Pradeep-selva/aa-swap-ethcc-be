16f4298276defdf1bab1239bcaf7a55ee826336e
Rahul Tripathi <rahultripathidev@gmail.com>
2023-07-22 13:15:11 +0530

    ft: add keeper abis

---
+++ src/constants.ts
@@ -0,0 +1,193 @@
+import { ethers } from "ethers";
+const SafeAAModuleABI = [
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "entryPointAddress",
+        type: "address",
+      },
+      {
+        internalType: "address",
+        name: "_registeredKeeper",
+        type: "address",
+      },
+    ],
+    stateMutability: "nonpayable",
+    type: "constructor",
+  },
+  {
+    inputs: [],
+    name: "InvalidSignature",
+    type: "error",
+  },
+  {
+    inputs: [],
+    name: "enableMyself",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "entryPoint",
+    outputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "to",
+        type: "address",
+      },
+      {
+        internalType: "uint256",
+        name: "value",
+        type: "uint256",
+      },
+      {
+        internalType: "uint8",
+        name: "callType",
+        type: "uint8",
+      },
+      {
+        internalType: "bytes",
+        name: "data",
+        type: "bytes",
+      },
+    ],
+    name: "execTransaction",
+    outputs: [],
+    stateMutability: "payable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "myAddress",
+    outputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        components: [
+          {
+            internalType: "address",
+            name: "sender",
+            type: "address",
+          },
+          {
+            internalType: "uint256",
+            name: "nonce",
+            type: "uint256",
+          },
+          {
+            internalType: "bytes",
+            name: "initCode",
+            type: "bytes",
+          },
+          {
+            internalType: "bytes",
+            name: "callData",
+            type: "bytes",
+          },
+          {
+            internalType: "uint256",
+            name: "callGasLimit",
+            type: "uint256",
+          },
+          {
+            internalType: "uint256",
+            name: "verificationGasLimit",
+            type: "uint256",
+          },
+          {
+            internalType: "uint256",
+            name: "preVerificationGas",
+            type: "uint256",
+          },
+          {
+            internalType: "uint256",
+            name: "maxFeePerGas",
+            type: "uint256",
+          },
+          {
+            internalType: "uint256",
+            name: "maxPriorityFeePerGas",
+            type: "uint256",
+          },
+          {
+            internalType: "bytes",
+            name: "paymasterAndData",
+            type: "bytes",
+          },
+          {
+            internalType: "bytes",
+            name: "signature",
+            type: "bytes",
+          },
+        ],
+        internalType: "struct UserOperation",
+        name: "userOp",
+        type: "tuple",
+      },
+      {
+        internalType: "bytes32",
+        name: "",
+        type: "bytes32",
+      },
+      {
+        internalType: "uint256",
+        name: "missingAccountFunds",
+        type: "uint256",
+      },
+    ],
+    name: "validateUserOp",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "validationData",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+];
+const SafeMultiSendABI = [
+  {
+    inputs: [
+      {
+        internalType: "bytes",
+        name: "transactions",
+        type: "bytes",
+      },
+    ],
+    name: "multiSend",
+    outputs: [],
+    stateMutability: "payable",
+    type: "function",
+  },
+];
+
+export const SafeAAModuleInterface = new ethers.utils.Interface(
+  SafeAAModuleABI
+);
+export const SafeMultiSendInterface = new ethers.utils.Interface(
+  SafeMultiSendABI
+);
