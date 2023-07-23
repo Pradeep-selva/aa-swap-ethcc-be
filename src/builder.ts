4ab4dd85812ed3046831c37cc324c680786cb4a8
Rahul Tripathi <rahultripathidev@gmail.com>
2023-07-23 05:43:02 +0530

    ft: add supported methods to builder

--- src/builder.ts
+++ src/builder.ts
@@ -15,30 +15,53 @@ export const BuildSafeCallData = (transactions: MetaTransaction[]): string => {
     const value = BigNumber.from(0);
     console.log(encodedTransaction);
     return SafeAAModuleInterface.encodeFunctionData("execTransaction", [
-      target,
-      value,
-      encodedTransaction.operation || 0,
-      encodedTransaction.data,
+      transactions[0].to,
+      transactions[0].value,
+       0,
+       transactions[0].data,
     ]);
   } catch (error) {
     console.log(error);
     return "";
   }
 };
+ const eip1559GasPrice = async (provider: ethers.providers.JsonRpcProvider) => {
+  const [fee, block] = await Promise.all([
+    provider.send("eth_maxPriorityFeePerGas", []),
+    provider.getBlock("latest"),
+  ]);
 
-export const BuildUserOP = (
+  const tip = ethers.BigNumber.from(fee);
+  const buffer = tip.div(100).mul(40);
+  const maxPriorityFeePerGas = tip.add(buffer);
+  const maxFeePerGas = block.baseFeePerGas
+    ? block.baseFeePerGas.mul(2).add(maxPriorityFeePerGas)
+    : maxPriorityFeePerGas;
+
+  return { maxFeePerGas, maxPriorityFeePerGas };
+};
+
+export const BuildUserOP = async (
   safeAddress: string,
   callData: string,
-  signature: string
-): UserOperationBuilder => {
+  signature: string,
+  nonce: number
+): Promise<UserOperationBuilder> => {
+  const rpc = new ethers.providers.JsonRpcProvider(
+    process.env.ALCHEMY_URL || ""
+  );
+  const gasprice = (await rpc.getGasPrice()).mul(BigNumber.from(2))
+  const {maxFeePerGas, maxPriorityFeePerGas} = await eip1559GasPrice(rpc)
+  
   return new UserOperationBuilder().useDefaults({
-    nonce:7,
+    nonce: BigNumber.from(nonce),
     sender: safeAddress,
     callData,
     signature,
-    maxFeePerGas: "4516700509",
+    maxFeePerGas: gasprice,
     preVerificationGas: "10000924",
-    maxPriorityFeePerGas: "203929",
+    callGasLimit:"300000",
+    maxPriorityFeePerGas: maxPriorityFeePerGas,
     verificationGasLimit: "1500000",
   });
 };
